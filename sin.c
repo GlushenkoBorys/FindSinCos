f59ba1f70adb2177e78c6aa25bbe5293c8efc06b
Borys <B.Glushchenko21@gmail.com>
2021-08-09 14:43:55 +0300

    test

---
+++ sin.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
+double getNumber(char stringIn []);
+void getSin(double firstNumber, double secondNumber, double increment);
+void getCos(double firstNumber, double secondNumber, double increment);
+void printSin(double glad);
+void printCos(double glad);
+
+int main (void)
+{
+	double firstNumber = getNumber("first");
+	
+	double secondNumber = getNumber("second");
+	
+	double increment = getNumber("increment");
+	 
+    getSin(firstNumber, secondNumber, increment);
+  	
+    getCos(firstNumber, secondNumber, increment);
+
+ return 0;
+}
+
+double getNumber(char stringIn [])
+{
+	double number;
+	printf("Input your %s number:\n", stringIn);
+    scanf("%lf", &number);
+	return number;
+}
+
+void getSin(double firstNumber, double secondNumber, double increment)
+{
+	for(double i = firstNumber; i <= secondNumber; i+= increment){
+   double glad = i;
+   
+	printSin(glad);
+	}
+}
+
+void getCos(double firstNumber, double secondNumber, double increment)
+{
+	for(double i = firstNumber; i <= secondNumber; i+= increment){
+   double glad = i;
+   
+	printCos(glad);
+	}
+}
+
+void printSin(double glad)
+{
+	printf("Argument: %f radians ", glad);
+    printf ("sin  : %.20f \n\n", sin(glad) );
+}
+
+void printCos(double glad)
+{
+	printf ("\n");
+	printf("Argument: %f radians ", glad);
+    printf ("cos  : %.20f \n\n", cos (glad) );
+}
+
+
