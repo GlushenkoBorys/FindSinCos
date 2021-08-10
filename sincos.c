f45af0590796843e1a1149c225a464c2a4b42364
Borys <B.Glushchenko21@gmail.com>
2021-08-10 22:04:07 +0300

    getting an array

--- sincos.c
+++ sincos.c
@@ -5,20 +5,26 @@
 #include <stdlib.h>
 
 double getNumber(char stringIn []);
-void getResult(double firstNumber, double secondNumber, double increment, void (*printSin)(double));
-void func ( void (*f)(int) );
-void printSin(double radians);
-void printCos(double radians);
+double * getResult(double firstNumber, double secondNumber, double increment, char (*string), double (*ptrMathFunc)(double));
+//void printResult(double radians, char (*string));
+void printResult(double radians);
+
 
 int main (void)
 {
+	
+
 	double firstNumber = getNumber("first");	
 	double secondNumber = getNumber("second");	
 	double increment = getNumber("increment");
-	 
-    getResult(firstNumber, secondNumber, increment, printSin);  
+	 	
+		
+	
+	double *ptrArray = getResult(firstNumber, secondNumber, increment, "sin", sin);  
+	printResult( *ptrArray);
     printf ("\n");	
-    getResult(firstNumber, secondNumber, increment, printCos);
+	
+    getResult(firstNumber, secondNumber, increment, "cos", cos);
 	
  return 0;
 }
@@ -31,28 +37,34 @@ double getNumber(char stringIn [])
 	return number;
 }
 
-void getResult(double firstNumber, double secondNumber, double increment, void (*function)(double))
+double * getResult(double firstNumber, double secondNumber, double increment, char * string, double (*ptrMathFunc)(double))
 {
+	int arrayLength = secondNumber - firstNumber;
+	double array[arrayLength];
+	double *ptrArray = array;
+	int j = 0;
 	for(double i = firstNumber; i <= secondNumber; i+= increment){
-   double radians = i;
+		
+		array[j] = ptrMathFunc(i);
+		j++;
+   //double result =ptrMathFunc(i);
    
-   function(radians);
+   //printResult(result, string);
    
 	}
+	return ptrArray;
 }
 
-void printSin(double radians)
+//void printResult(double radians, char (*string))
+//{
+//	printf ("\n");
+//	printf("Argument: %f radians    ", radians);
+ //   printf ("%s : %.20f", string, radians);
+//}
+
+void printResult(double radians)
 {
 	printf ("\n");
 	printf("Argument: %f radians    ", radians);
-    printf ("sin  : %.20f", sin(radians) );
+    printf ("%.20f", radians);
 }
-
-void printCos(double radians)
-{
-	printf ("\n");
-	printf("Argument: %f radians    ", radians);
-    printf ("cos  : %.20f", cos (radians) );
-}
-
-
