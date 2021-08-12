8416ca6bc3bde05d7b6ab0b6bea28ed511878978
Borys <B.Glushchenko21@gmail.com>
2021-08-12 20:33:17 +0300

    working version with error double

--- sincos.c
+++ sincos.c
@@ -5,26 +5,29 @@
 #include <stdlib.h>
 
 double getNumber(char stringIn []);
-double * getResult (double *ptrArray, double firstNumber, double secondNumber, double increment, double (*ptrMathFunc)(double));
-void printResult(double ptrArray [], char string [], double firstNumber, double secondNumber, double increment);
+double * getResult (int count, double *ptrArray, double firstNumber, double secondNumber, double increment, double (*ptrMathFunc)(double));
+void printResult(int count, double ptrArray [], char string [], double firstNumber, double secondNumber, double increment);
 
 
 int main (void)
 {
-
 	double firstNumber = getNumber("first");	
 	double secondNumber = getNumber("second");	
 	double increment = getNumber("increment");
 
-	int arrayLength = ((secondNumber - firstNumber) + 1.0 ) / increment;
+	int arrayLength = (((secondNumber - firstNumber) / (increment)) + 1);
+	
+	printf("this %d:\n", arrayLength);
+
 	double array[arrayLength];
 	double *ptrArray = array;
+	printf ("\n");
 	 		
-	ptrArray = getResult(ptrArray, firstNumber, secondNumber, increment, sin);  
-	printResult( ptrArray, "sin", firstNumber, secondNumber, increment);
+	ptrArray = getResult(arrayLength, ptrArray, firstNumber, secondNumber, increment, sin);  
+	printResult(arrayLength, ptrArray, "sin", firstNumber, secondNumber, increment);
     printf ("\n");	
-	ptrArray = getResult(ptrArray, firstNumber, secondNumber, increment, cos);  
-	printResult( ptrArray, "cos", firstNumber, secondNumber, increment);
+	ptrArray = getResult(arrayLength, ptrArray, firstNumber, secondNumber, increment, cos);  
+	printResult(arrayLength, ptrArray, "cos", firstNumber, secondNumber, increment);
 	
  return 0;
 }
@@ -37,24 +40,30 @@ double getNumber(char stringIn [])
 	return number;
 }
 
-double * getResult (double *ptrArray, double firstNumber, double secondNumber, double increment, double (*ptrMathFunc)(double))
+double * getResult (int count, double *ptrArray, double firstNumber, 
+double secondNumber, double increment, double (*ptrMathFunc)(double))
 {
 	int j = 0;
-	for(double i = firstNumber; i <= secondNumber; i+= increment){
+	for(double i = firstNumber ; ; i+= increment, j++){
 		
-		ptrArray[j] = ptrMathFunc(i);
-		j++;
+		if (j == count)
+					break;
+		
+		ptrArray[j] = ptrMathFunc(i);		
 	}
 	return ptrArray;
 }
 
-void printResult(double ptrArray [], char string [], double firstNumber, double secondNumber, double increment)
+void printResult(int count, double ptrArray [], char string [], double firstNumber, 
+double secondNumber, double increment)
 {
 	int j = 0;
-	for (double i = firstNumber; i <= secondNumber; i+= increment){
-
+	for (double i = firstNumber; ; i+= increment, j++){
+		
+        if (j == count)
+					break;
+		
 		printf("Argument: %f radians    ", i);
-	    printf ("%s: %.20f\n",string, ptrArray [j]);
-		j++;
+	    printf ("%s: %.20f\n",string, ptrArray [j]);		
 	}	
 }
