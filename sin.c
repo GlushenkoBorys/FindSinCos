baf4d95d5cc0a37fbdb6c5f9947cc9880fc5f8e3
Borys <B.Glushchenko21@gmail.com>
2021-08-14 12:27:08 +0300

    work version find sin cos

--- sin.c
+++ sin.c
@@ -3,25 +3,26 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sin.c>
 
 double getNumber(char stringIn []);
-void getSin(double firstNumber, double secondNumber, double increment);
-void getCos(double firstNumber, double secondNumber, double increment);
-void printSin(double glad);
-void printCos(double glad);
+double getSin(double sinCosNumber);
+double getCos(double sinCosNumber);
+int factorial(int n);
 
 int main (void)
 {
-	int flag = 0;
-	double firstNumber = getNumber("first");	
-	double secondNumber = getNumber("second");	
-	double increment = getNumber("increment");
-	 
-    getSin(firstNumber, secondNumber, increment);  	
-    //getCos(firstNumber, secondNumber, increment);
+   double sinCosNumber = getNumber("sinCosNumber");
+   //int numberDegree = getNumber("degree");
+   
+   double result = getSin(sinCosNumber);
+   printf ("result %.20f\n", result);
+   printf ("sin  : %.20f \n", sin(1.23) );
+   result = getCos(sinCosNumber);
+   printf ("result %.20f\n", result);
+   printf ("cos  : %.20f \n", cos(1.23) );
 
- return 0;
+return 0;
 }
 
 double getNumber(char stringIn [])
@@ -29,37 +30,45 @@ double getNumber(char stringIn [])
 	double number;
 	printf("Input your %s number:\n", stringIn);
     scanf("%lf", &number);
+
 	return number;
 }
 
-void getSin(double firstNumber, double secondNumber, double increment)
+double getSin(double sinCosNumber)
 {
-	for(double i = firstNumber; i <= secondNumber; i+= increment){
-   double glad = i;
-   
-	printSin(glad);
+	double sin = 0;
+	double n = 1;
+
+	for(int i = 0; i < 15; i++, n++) {
+		
+	sin += (((pow((double)(-1),(double)(n + 1))) * (pow((double)sinCosNumber,(double)(2 * n - 1))))
+	/ factorial(2 * n - 1));
+
+	}	
+	return sin;
+}
+
+double getCos(double sinCosNumber)
+{
+	double cos = 0;
+	double n = 0;	
+
+	for(int i = 0; i < 15; i++, n++) {
+		
+	cos += (((pow((double)(-1),(double)(n))) * (pow((double)sinCosNumber,(double)(2 * n ))))
+	/ (factorial(2 * n )));
+
+	}	
+	return cos;
+}
+
+int factorial(int n)
+{
+    if (n == 1 || n == 0){
+	   return 1;
 	}
+	   else {   
+	   n = n * factorial(n - 1);
+	   return n;
+	   }
 }
-
-//void getCos(double firstNumber, double secondNumber, double increment)
-//{
-//	for(double i = firstNumber; i <= secondNumber; i+= increment){
-//   double glad = i;   
-//	printCos(glad);
-//	}
-//}
-
-void printSin(double glad)
-{
-	printf("Argument: %f radians ", glad);
-    printf ("sin  : %.20f \n\n", sin(glad) );
-}
-
-void printCos(double glad)
-{
-	printf ("\n");
-	printf("Argument: %f radians ", glad);
-    printf ("cos  : %.20f \n\n", cos (glad) );
-}
-
-
