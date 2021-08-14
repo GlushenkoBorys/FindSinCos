7493402239cda18a4bc37b74ef3aaf6ebbb7d04f
Borys <B.Glushchenko21@gmail.com>
2021-08-14 15:01:23 +0300

    precision input program

--- sin.c
+++ sin.c
@@ -3,24 +3,24 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <stdlib.h>
-#include <sin.c>
+
 
 double getNumber(char stringIn []);
-double getSin(double sinCosNumber);
-double getCos(double sinCosNumber);
+double getSin(double sinCosNumber, int numberDegree);
+double getCos(double sinCosNumber, int numberDegree);
 int factorial(int n);
 
 int main (void)
 {
    double sinCosNumber = getNumber("sinCosNumber");
-   //int numberDegree = getNumber("degree");
+   int numberDegree = getNumber("degree");
    
-   double result = getSin(sinCosNumber);
+   double result = getSin(sinCosNumber, numberDegree);
    printf ("result %.20f\n", result);
-   printf ("sin  : %.20f \n", sin(1.23) );
-   result = getCos(sinCosNumber);
+   printf ("sin  : %.20f \n", sin(sinCosNumber) );
+   result = getCos(sinCosNumber, numberDegree);
    printf ("result %.20f\n", result);
-   printf ("cos  : %.20f \n", cos(1.23) );
+   printf ("cos  : %.20f \n", cos(sinCosNumber) );
 
 return 0;
 }
@@ -34,28 +34,30 @@ double getNumber(char stringIn [])
 	return number;
 }
 
-double getSin(double sinCosNumber)
+double getSin(double sinCosNumber, int numberDegree)
 {
 	double sin = 0;
 	double n = 1;
 
-	for(int i = 0; i < 15; i++, n++) {
+	for(int i = 0; i < numberDegree; i++, n++) {
 		
-	sin += (((pow((double)(-1),(double)(n + 1))) * (pow((double)sinCosNumber,(double)(2 * n - 1))))
+	sin += (((pow((double)(-1),(double)(n + 1))) *
+	(pow((double)sinCosNumber,(double)(2 * n - 1))))
 	/ factorial(2 * n - 1));
 
 	}	
 	return sin;
 }
 
-double getCos(double sinCosNumber)
+double getCos(double sinCosNumber, int numberDegree)
 {
 	double cos = 0;
 	double n = 0;	
 
-	for(int i = 0; i < 15; i++, n++) {
+	for(int i = 0; i < numberDegree; i++, n++) {
 		
-	cos += (((pow((double)(-1),(double)(n))) * (pow((double)sinCosNumber,(double)(2 * n ))))
+	cos += (((pow((double)(-1),(double)(n))) *
+	(pow((double)sinCosNumber,(double)(2 * n ))))
 	/ (factorial(2 * n )));
 
 	}	
