8ae7af3a0abc8fed007acd11d2152f1ad13d32a3
Borys <B.Glushchenko21@gmail.com>
2021-08-20 19:01:04 +0300

    program with test -23sec

--- SinCosResult.h
+++ SinCosResult.h
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdbool.h>
-#include <limits.h>
-#include <stdlib.h>
 
 double getSin(double sinCosNumber);
 double getCos(double sinCosNumber);
@@ -15,15 +10,25 @@ double getSin(double sinCosNumber)
 {
 	double sin = 0;
 	double n = 1;
+	double result = sinCosNumber;
+	double powResult;
 	
 	sinCosNumber = findMinAfterTwoPi(sinCosNumber);
 
-	for(int i = 0; i < numberDegree; i++, n++) {
+	for(int i = 1; i < numberDegree; i++, n++) {
 		
-	sin += ((pow((double)(-1),(double)(n + 1))) *
-	(pow((double)sinCosNumber,(double)(2 * n - 1))))
-	/ ((double)(factorial(2 * n - 1)));
-
+    result *= (sinCosNumber * sinCosNumber);
+	
+	powResult *= (-1.0 * 1.0);
+	
+	if( i == 1 )
+		powResult = 1;
+	
+	if( i == 1 )
+	    result = sinCosNumber;
+	
+	sin += powResult * result / ((factorial(2 * n - 1)));
+	
 	}	
 	return sin;
 }
@@ -32,17 +37,35 @@ double getCos(double sinCosNumber)
 {
 	double cos = 0;
 	double n = 0;	
+	double result = 1;
+	double powResult;
+	double resultFact;
 	
     sinCosNumber = findMinAfterTwoPi(sinCosNumber);
-
+	
 	for(int i = 0; i < numberDegree; i++, n++) {
+	
+	result *= (sinCosNumber * sinCosNumber);
+	
+	powResult *= (-1.0 * 1.0);
+	
+	resultFact = ((double)(factorial(2 * n )));
+	
+	if( i == 0 )
+		powResult = 1;
+	
+	if( i == 0 )
+	   result = 1;
+   
+    if( i == 0)
+	    resultFact = 1;
 		
-	cos += ((pow((double)(-1),(double)(n))) *
-	(pow((double)sinCosNumber,(double)(2 * n ))))
-	/ ((double)(factorial(2 * n )));
-
+	cos += powResult * result / resultFact;
+	
+	 
+	
 	}	
-	//
+	
 	return cos;
 }
 
@@ -57,13 +80,10 @@ unsigned long long int factorial(unsigned long long int n)
 	   }
 }
 
-double findMinAfterTwoPi(double number) {
-	
-	if(number > twoPi) {	   
-	 while( number > twoPi )
-	 {
-		 number -= twoPi;
-	 }	   
-   }
-   return number;
+double findMinAfterTwoPi(double sinCosNumber) { 	   
+   
+   double number = sinCosNumber / twoPi;
+   double result = sinCosNumber - (twoPi * (int)number);
+      
+   return result;
 }
